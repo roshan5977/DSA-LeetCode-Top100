3e453746421f2010ac9c47dc378d74b815918843
roshan5977 <roshannayak5977@gmail.com>
2024-05-06 22:19:49 +0530

    d

---
+++ indexOfFirstOccurrence.java
@@ -0,0 +1,35 @@
+
+
+
+/**
+ * Given two strings needle and haystack, 
+ * return the index of the first occurrence of needle in haystack,
+ *  or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+*/
+
+public class indexOfFirstOccurrence {
+    public static void main(String[] args) {
+        String haystack="bussawdtsasd";
+        String needle="sad";
+        System.out.println(strStr(haystack, needle));
+    }
+
+      static int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+
+
+
+}
