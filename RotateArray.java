d2edb776bc2142f364d46993fdeb6148e95da1fd
roshan5977 <roshannayak5977@gmail.com>
2024-05-13 22:40:46 +0530

    d

--- RotateArray.java
+++ RotateArray.java
@@ -6,6 +6,7 @@
  * For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
  * 
  * How many different ways do you know to solve this problem?
+ * ,k
  */
 
 public class RotateArray {
